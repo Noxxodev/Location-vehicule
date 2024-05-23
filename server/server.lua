40a8d16dd47fcf08303d2e7a86d7b809abfe2586
Noxxo Dev <148816839+Noxxodev@users.noreply.github.com>
2024-05-23 21:00:54 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,29 +1,8 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-
-RegisterNetEvent('nox_loca:vehicule')
-AddEventHandler('nox_loca:vehicule', function(prix)
+RegisterNetEvent('nox_location:prix')
+AddEventHandler('nox_location:prix', function(prix)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local playerMoney = xPlayer.getMoney()
-	local min = Config.time / 60
 	xPlayer.removeMoney(prix)
-	--TriggerClientEvent('esx:showNotification', source, 'Tu viens de louer un véhicule pour ' ..min.. 'min.')
-	--TriggerClientEvent('esx:showNotification', source, 'Tu viens de louer un véhicule pour ' ..prix.. '$.')
-	TriggerClientEvent("s1n_notify:notify", xPlayer.source, {
-		type = 'info',
-		title = 'Location',
-		message = 'Tu viens de louer un véhicule pour ' ..min.. 'min.',
-		duration = 8000,
-		theme = 'white',
-		position = 'top-left'
-	  })
-	  TriggerClientEvent("s1n_notify:notify", xPlayer.source, {
-		type = 'info',
-		title = 'Location',
-		message = 'Tu viens de louer un véhicule pour ' ..prix.. '$.',
-		duration = 8000,
-		theme = 'white',
-		position = 'top-left'
-	  })
-
 end)
+
